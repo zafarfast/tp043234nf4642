524805beca130dc099b1cdcc35d2c6944b84b8f2
Bryan S <72003881+momentes@users.noreply.github.com>
2023-05-06 14:17:48 +1000

    Push Update - UserProfile

--- client/src/components/userProfile.js
+++ client/src/components/userProfile.js
@@ -1,32 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from './footer';
 
+import { GET_USER } from '../utils/queries';
+import { EDIT_USER } from '../utils/mutations';
+import { useMutation, useQuery } from '@apollo/client';
 
-export default function //Signup() {
+import Auth from "../utils/auth"
+
+
+export default function UserProfile() {
+
+    const {loading, data} = useQuery(GET_USER, {
+        variables: {
+            email: "zafar123@hotmail.com"
+            // email: Auth.getUser().email
+        }
+    })
+
+    const userData = data?.findUser || {}
+
+    const [formData, setFormData] = useState({
+        "userId":"",
+        "email":"", 
+        "firstName":"",
+        "lastName":"",
+        "password": ""
+    })
+
+
+    useEffect(() => {
+        setFormData({
+            "userId": userData._id,
+            "email": userData.email, 
+            "firstName": userData.firstName,
+            "lastName": userData.lastName,
+            "password": ""
+        })
+    }, [userData])
+
+    const [editUser] = useMutation(EDIT_USER)
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+
+        await editUser({
+            variables: {
+                "userId": formData.userId,
+                "email": formData.email, 
+                "firstName": formData.firstName,
+                "lastName": formData.lastName,
+                "password": formData.password
+            }
+        })
+
+        window.location.href = "/userProfile"
+
+    }
+
+    function handleChange (event) {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+
+        console.log(formData)
+    }
+
+    if(loading) {
+        return (
+            <h2>Still loading, please wait!</h2>
+        )
+    }
+
     return <>
         <div id="signup-form-container">
-            <h1 id="thoughtbook-title-text-login-page">ThoughtBook <img id="logo-login-page" src="./images/comment.png" alt=""></img></h1>
+            <h1 id="thoughtbook-title-text-login-page">Update User <img id="logo-login-page" src="./images/comment.png" alt=""></img></h1>
             <br></br>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label for="email">First Name</label>
                 <br></br>
-                <input type="text"></input>
+                <input type="text" name="firstName" value={formData.firstName} onChange={handleChange}></input>
                 <br></br>
                 <br></br>
                 <label for="email">Last Name</label>
                 <br></br>
-                <input type="text"></input>
+                <input type="text"  name="lastName" value={formData.lastName} onChange={handleChange}></input>
                 <br></br>
                 <br></br>
                 <label for="email">Email</label>
                 <br></br>
-                <input type="text"></input>
+                <input type="text"  name="email" value={formData.email} onChange={handleChange}></input>
                 <br></br>
                 <br></br>
                 <label for="password">Password</label>
                 <br></br>
 
-                <input type="password"></input>
+                <input type="password" name="password" onChange={handleChange}></input>
                 <br></br>
                 <br></br>
                 <button id="login-button">Save Edit</button>
